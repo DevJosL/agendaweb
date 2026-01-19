b634e93b0399318df1fd44b7b40c1d25110b2d3a
DevJosL <jmoran-2022388@kinal.edu.gt>
2026-01-19 15:51:40 -0600

    Se arreglo un error en el almacenamiento local que hacia que se reescribiera la información, se probó y se demsotro el correcto funcionamineto.

--- scr/components/sections/contactos/db.js
+++ scr/components/sections/contactos/db.js
@@ -1,7 +1,12 @@
-let ContactList = [
+import { getContactcsFromStorage, saveContatcsToStorage } from "./Storage.js";
+
+let ContactList = getContactcsFromStorage();
+saveContatcsToStorage(ContactList);
+
+/* let ContactList = [
     {nombre: "Juan", telefono: "12345678"},
     {nombre: "Maria", telefono: "12345678"},
     {nombre: "Pedro", telefono: "12345678"},
-];
+]; */
 
 export {ContactList};
